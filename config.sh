802801dbe5e357a3289c2f31268b80fdbed73b80
medesd <60796424+medesd@users.noreply.github.com>
2023-07-13 15:29:42 +0100

    Update config.sh
    update twrp repo

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
 export DEVICE="j7duolte" # codename used in device tree
-export DT_LINK="https://github.com/medesd/android_recovery_samsung_j7duolte" # device tree link
+export DT_LINK="https://github.com/Griffin98/twrp_samsung_j7duolte" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
